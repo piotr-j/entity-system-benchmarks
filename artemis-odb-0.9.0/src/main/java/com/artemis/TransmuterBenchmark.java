c47691f5f38d43e6b688cc86134cf4c566d40287
Adrian Papari <blackrax@gmail.com>
2015-01-22 01:56:03 +0100

    entity transmuter benchmark, removed unused exp module.

---
+++ artemis-odb-0.9.0/src/main/java/com/artemis/TransmuterBenchmark.java
@@ -0,0 +1,37 @@
+package com.artemis;
+
+import com.artemis.system.*;
+import com.github.esfbench.JmhSettings;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+public class TransmuterBenchmark extends JmhSettings {
+	
+	private World world;
+
+	@Setup
+	public void init() {
+		world = new World();
+		world.setSystem(new BaselinePositionSystem());
+		world.setSystem(new BaselinePositionSystem2());
+		world.setSystem(new BaselinePositionSystem3());
+		world.setSystem(new CompositionManglerSystem(SEED, entityCount));
+		world.initialize();
+	}		
+	
+	@Benchmark
+	public void baseline() {
+		world.process();
+	}
+
+	public static void main(String[] args) throws Exception {
+		new Runner(
+			new OptionsBuilder()
+				.include(TransmuterBenchmark.class.getName() + ".*")
+				.param("entityCount", "1024", "4096")
+				.build())
+		.run();
+	}
+}
